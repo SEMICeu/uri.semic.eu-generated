91a9ca914090665a313082cf78725b190e3b7f31
Circle CI Builder <contact@semic.eu>
2024-01-19 14:01:03 +0000

    Applying changes from commit 08eeb38c6ec84ebafde9692c01814d9214242ea4

--- DCAT-AP/releases/3.0.0/html/example.js
+++ DCAT-AP/releases/3.0.0/html/example.js
@@ -128,10 +128,26 @@ function validate(model, version, content, format) {
 				dialog.closest(".ui-dialog").children(".ui-dialog-titlebar").css({"background": "red", "background-image": "linear-gradient(to bottom,#ac6464,#b31c1c)","color":"white"});
 		},
 
-		error: function (jqXHR, status) {
+		error: function (jqXHR, exception) {
 		 // error handler
-		 console.log(jqXHR);
-		 alert('fail' + status.code);
+		var msg = '';
+		if (jqXHR.status === 0) {
+			msg = 'Not connect.\n Verify Network.';
+		} else if (jqXHR.status == 404) {
+			msg = 'Requested page not found. [404]';
+		} else if (jqXHR.status == 500) {
+			msg = 'Internal Server Error [500].';
+		} else if (exception === 'parsererror') {
+			msg = 'Requested JSON parse failed.';
+		} else if (exception === 'timeout') {
+			msg = 'Time out error.';
+		} else if (exception === 'abort') {
+			msg = 'Ajax request aborted.';
+		} else {
+			msg = 'Uncaught Error.\n' + jqXHR.responseText;
+		}
+		
+		alert('It was not possible to validate, error:' + msg);
 		}
 		});
 }
